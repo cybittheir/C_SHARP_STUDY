c7d1f50e166a2bbddc56ae141fded86c69f198bd
cybittheir <kibitzer.vl@hotmail.com>
2023-03-24 00:26:04 +1000

    cshart first seminar all tasks in one

--- st_1_h2/Program.cs
+++ st_1_h2/Program.cs
@@ -2,9 +2,11 @@
 Console.Write("Сколько чисел сравниваем (2-5): ");
 int count =1;
 
-while (count<2||count>5){
+while (count<2||count>5)
+{
     count=int.Parse(Console.ReadLine());
-    if (count<2||count>5){
+    if (count<2||count>5)
+    {
         Console.Write("Ошибка. Повторите ввод: ");
     }
 }
@@ -12,37 +14,64 @@ while (count<2||count>5){
 int i;
 int [] compare =new int[count];
 
-for (i=0;i<count;i++){
-    Console.Write($"Введите число №{i+1}:");
+for (i=0;i<count;i++)
+{
+    Console.Write($"Введите число №{i+1}: ");
     compare[i]=int.Parse(Console.ReadLine());
 }
 
 int max;
 int min;
 
-if (compare[0]>compare[count-1]){
+if (compare[0]>compare[count-1])
+{
         max=compare[0];
         min=compare[count-1];
-} else {
+} 
+else 
+{
         min=compare[0];
         max=compare[count-1];
 }
 
 string numbers="";
 
-for (i=0;i<count;i++){
-    if (compare[i]>max){
+for (i=0;i<count;i++)
+{
+    if (compare[i]>max)
+    {
         max=compare[i];
-    } else if (compare[i]<min){
+    }
+    else if (compare[i]<min)
+    {
         min=compare[i];
     }
-    if (i<count-1){
+    if (i<count-1)
+    {
         numbers+=compare[i].ToString()+",";
-    } else {
+    }
+    else
+    {
         numbers+=compare[i].ToString();
+    }
+}
+
+int j;
+string odds="";
+
+for (j=min;j<=max;j++)
+{
+    if (j%2==0 && odds=="")
+    {
+        odds=j.ToString();
+    }
+    else if (j%2==0)
+    {
+        odds+=", "+j.ToString();
     }
 }
 
 Console.WriteLine("Числа: " + numbers);
 Console.WriteLine($"Минимум: {min}");
 Console.WriteLine($"Максимум: {max}");
+Console.WriteLine($"Все четные числа между крайними значениями(включая их): {odds}");
