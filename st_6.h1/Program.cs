bd65de14b55f038a222775ac3441ddaa0b9d7218
cybittheir <kibitzer.vl@hotmail.com>
2023-04-10 23:25:37 +1000

    Lesson 6 HW

---
+++ st_6.h1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+*/
+
+void calcPositive(string [] array) {
+    int result=0;
+    foreach(string val in array) {
+        if (Convert.ToInt32(val)>0){
+            result++;
+        }
+    }
+    Console.WriteLine(result);
+}
+
+string [] inputArray(string prompt) {
+
+    Console.Write($"{prompt}:");
+    string [] newArray=Console.ReadLine().Split(",");
+    Console.Write(String.Join("; ",newArray) + " -> ");
+    return newArray;
+}
+
+calcPositive(inputArray("Введите числе через запятую"));
---
+++ st_6.h2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*/
+
+double [] findPoint(int b1,int k1,int b2, int k2){
+
+    double x = (Convert.ToDouble(b2)-Convert.ToDouble(b1))/(Convert.ToDouble(k1)-Convert.ToDouble(k2));
+    double y = Convert.ToDouble(k1)*x + Convert.ToDouble(b1);
+    double[]result={x,y};
+    return result;
+}
+
+int a=6;
+int b=3;
+int c=4;
+int d=9;
+
+Console.Write($"b1 = {a}, k1 = {b}, b2 = {c}, k2 = {d} -> ");
+double [] point=findPoint(a,b,c,d);
+Console.WriteLine($"({point[0]:f2}; {point[1]:f2})");
